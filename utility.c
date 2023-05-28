b189fce2d98e1d1dac545d11caff13531426dac9
AmaChina <nanaama91@hotmail.com>
2023-05-28 15:38:53 -0700

    utility

--- utility.c
+++ utility.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
  * is_printable - Gauges if char is presumable
- * @c: Character gauged.
- *
- * Return: 1 if f is presume, 0 otherwise
+ * @k: Character gauged
+ * Return: 1 if k is presume, 0 otherwise
  */
-int is_printable(char f)
+int is_printable(char k)
 {
-	if (f >= 32 && f < 127)
+	if (k >= 32 && k < 127)
 		return (1);
 
 	return (0);
@@ -17,35 +15,34 @@ int is_printable(char f)
 /**
  * append_hexa_code - Attach ascci in hexadecimal code to buffer
  * @buffer: chars array
- * @i: Hint at which to begin adjoining.
+ * @n: Hint at which to begin adjoining.
  * @ascii_code: assci code.
  * Return: Constantly 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int p)
+int append_hexa_code(char ascii_code, char buffer[], int n)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[p++] = '\\';
-	buffer[p++] = 'x';
+	buffer[n++] = '\\';
+	buffer[n++] = 'x';
 
-	buffer[p++] = map_to[ascii_code / 16];
-	buffer[p] = map_to[ascii_code % 16];
+	buffer[n++] = map_to[ascii_code / 16];
+	buffer[n] = map_to[ascii_code % 16];
 
 	return (3);
 }
 
 /**
  * is_digit - Confirm char is a digit
- * @c: The char to be gauge
- *
+ * @k: The char to be gauge
  * Return: 1 if f is a digit, 0 otherwise
  */
-int is_digit(char f)
+int is_digit(char k)
 {
-	if (f >= '0' && f <= '9')
+	if (k >= '0' && k <= '9')
 		return (1);
 
 	return (0);
@@ -53,9 +50,8 @@ int is_digit(char f)
 
 /**
  * convert_size_number - Emits a number to the defined size
- * @num: Number to be emitted.
- * @size: Number showing type emit.
- *
+ * @num: Number to be emitted
+ * @size: Number showing type emit
  * Return: Evacuated value num
  */
 long int convert_size_number(long int num, int size)
@@ -72,7 +68,6 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Eject a number to defined size
  * @num: Number to eject
  * @size: Number showing the type to excude
- *
  * Return: Discharged value num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
