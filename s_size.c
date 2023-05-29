13bcdd722e624c29ea04819f0456b1d00309fef4
AmaChina <nanaama91@hotmail.com>
2023-05-29 02:58:37 -0700

    size

---
+++ s_size.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_size - compute size for argument
+ * @format: string format
+ * @n: argument list
+ * Return: size
+ */
+int get_size(const char *format, int *n)
+{
+	int new_n = *n + 1;
+	int size = 0;
+
+	if (format[new_n] == 'l')
+		size = S_LONG;
+	else if (format[new_n] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*n = new_n - 1;
+	else
+		*n = new_n;
+
+	return (size);
+}
