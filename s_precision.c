97705c6046408d71dbb5b9d6f3bd5146a83556f4
Ellabissa <bissahfrimpongemmanuella@gmail.com>
2023-05-30 14:03:59 -0700

    corrections

--- s_precision.c
+++ s_precision.c
@@ -36,28 +36,3 @@ int get_precision(const char *format, int *n, va_list list)
 	*n = new_n - 1;
 	return (precision);
 }
-
-#include "main.h"
-/**
- * get_size - computes size to arguments
- * @format: Constructed string to inscribe arguments
- * @n: argument list to be inscribed
- * Return: precision
- */
-int get_size(const char *format, int *n)
-{
-	int new_n = *n + 1;
-	int size = 0;
-
-	if (format[new_n] == 'l')
-		size = S_LONG;
-	else if (format[new_n] == 'n')
-		size = S_SHORT;
-
-	if (size == 0)
-		*n = new_n - 1;
-	else
-		*n = new_n;
-
-	return (size);
-}
